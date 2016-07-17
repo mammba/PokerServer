f34f2eb8ef29ca68ae6696c7dde91eb7b09d9b7a
igevorse <igevorse@gmail.com>
2016-07-17 23:10:08 +0300

    Chat Launcher demo server

---
+++ src/main/java/com/corundumstudio/socketio/demo/ChatLauncher.java
@@ -0,0 +1,37 @@
+package com.corundumstudio.socketio.demo;
+
+import com.corundumstudio.socketio.listener.*;
+import com.corundumstudio.socketio.*;
+
+public class ChatLauncher {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Configuration config = new Configuration();
+        config.setHostname("localhost");
+        config.setPort(8080);
+        config.setOrigin("http://localhost");
+
+        final SocketIOServer server = new SocketIOServer(config);
+
+        server.addConnectListener(new ConnectListener() {
+            public void onConnect(SocketIOClient client) {
+                System.out.println("Client connected.");
+            }
+        });
+        server.addEventListener("chatevent", ChatObject.class, new DataListener<ChatObject>() {
+            public void onData(SocketIOClient client, ChatObject data, AckRequest ackRequest) {
+                // broadcast messages to all clients
+                System.out.println("Got message");
+                server.getBroadcastOperations().sendEvent("chatevent", data);
+            }
+        });
+
+        server.start();
+
+        Thread.sleep(Integer.MAX_VALUE);
+
+        server.stop();
+    }
+
+}
