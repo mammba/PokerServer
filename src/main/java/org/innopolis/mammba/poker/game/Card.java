5d380ff1d879fa1995ed410cd500349ea7fb77ab
Mike <lubinetsm@yandex.ru>
2016-07-18 02:20:08 +0300

    Merged card

--- src/main/java/org/innopolis/mammba/poker/game/Card.java
+++ src/main/java/org/innopolis/mammba/poker/game/Card.java
@@ -1,4 +1,48 @@
 package org.innopolis.mammba.poker.game;
 
+// NOTE!!!
+// THIS IS TEMPORARY FILE
+// REMOVE ON MERGE WITH ANTON
+
 public class Card {
+    private Suit suit;
+    private Rank rank;
+
+    public Card(Suit nSuit, Rank nRank){
+        suit = nSuit;
+        rank = nRank;
+    }
+
+    public int compareTo(Card card){
+        if(this.getPriority() > card.getPriority()){
+            return 1;
+        }else if(this.getPriority() < card.getPriority()){
+            return -1;
+        }else{
+            return 0;
+        }
+    }
+
+    public int getPriority(){
+        int i = 1;
+        for (Rank rank : Rank.values()) {
+            if(rank == this.rank){
+                break;
+            }
+            i++;
+        }
+        return i;
+    }
+
+    public String getNotation() {
+        return suit.toString() + rank.toString();
+    }
+}
+
+enum Suit {
+    Hearts, Diamonds, Spades, Clubs;
+}
+
+enum Rank {
+    Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace;
 }
