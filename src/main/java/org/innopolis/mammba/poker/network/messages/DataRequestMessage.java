f8815724dff43adbb98b43500359be384bf522ef
Mike <lubinetsm@yandex.ru>
2016-07-20 18:11:53 +0300

    #Poker-51: Message and data type in abstract message

--- src/main/java/org/innopolis/mammba/poker/network/messages/DataRequestMessage.java
+++ src/main/java/org/innopolis/mammba/poker/network/messages/DataRequestMessage.java
@@ -1,8 +1,13 @@
 package org.innopolis.mammba.poker.network.messages;
 import org.innopolis.mammba.poker.network.messages.data.DataRequestData;
+import org.innopolis.mammba.poker.network.messages.data.MessageDataType;
 
 public class DataRequestMessage extends AbstractMessage {
     private DataRequestData data;
+
+    public DataRequestMessage() {
+        super(MessageType.DATA_REQUEST, MessageDataType.DATA_REQUEST);
+    }
 
     public DataRequestData getData()          { return data;}
     public void setData(DataRequestData data) { this.data = data; }
--- src/main/java/org/innopolis/mammba/poker/network/messages/TableStateUpdateMessage.java
+++ src/main/java/org/innopolis/mammba/poker/network/messages/TableStateUpdateMessage.java
@@ -1,9 +1,14 @@
 package org.innopolis.mammba.poker.network.messages;
 
+import org.innopolis.mammba.poker.network.messages.data.MessageDataType;
 import org.innopolis.mammba.poker.network.messages.data.TableStateData;
 
 public class TableStateUpdateMessage extends StateUpdateMessage {
     private TableStateData data;
+
+    public TableStateUpdateMessage() {
+        super(MessageDataType.TABLE_STATE);
+    }
 
     public TableStateData getData()                     { return data; }
     public void           setData(TableStateData data ) { this.data = data; }
