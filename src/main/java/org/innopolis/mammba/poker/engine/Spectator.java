c96a3c928b2f48dc37659856c28438822e334632
Mike <lubinetsm@yandex.ru>
2016-07-20 18:14:21 +0300

    #Poker-63: Storing player and spectator refs in User for listeners

--- src/main/java/org/innopolis/mammba/poker/engine/Spectator.java
+++ src/main/java/org/innopolis/mammba/poker/engine/Spectator.java
@@ -5,10 +5,13 @@ import  java.util.UUID;
 public class Spectator {
     private User user;
     private Room room;
+
     public Spectator(User user, Room room) {
         this.user = user;
         this.room = room;
+        user.setSpectator(this);
     }
+
     public User getUser() {
         return user;
     }
--- src/main/java/org/innopolis/mammba/poker/engine/User.java
+++ src/main/java/org/innopolis/mammba/poker/engine/User.java
@@ -2,6 +2,8 @@ package org.innopolis.mammba.poker.engine;
 
 import com.corundumstudio.socketio.SocketIOClient;
 import org.innopolis.mammba.poker.engine.game.Game;
+import org.innopolis.mammba.poker.engine.player.Player;
+import org.innopolis.mammba.poker.engine.player.PlayerAction;
 import org.innopolis.mammba.poker.network.messages.MessageType;
 import org.innopolis.mammba.poker.network.messages.TableStateUpdateMessage;
 import org.innopolis.mammba.poker.network.messages.data.TableStateData;
@@ -11,11 +13,13 @@ import java.util.List;
 import java.util.UUID;
 
 public class User {
-    private UUID uuid;
+    private UUID           uuid;
     private SocketIOClient client;
-    private String nickname;
-    private int money;
-    private static Long userCounter = 0L;
+    private String         nickname;
+    private int            money;
+    private Spectator      spectator;
+    private Player         player;
+    private static Long    userCounter = 0L;
 
     /**
      * Default constructor for an anonymous user.
@@ -27,54 +31,106 @@ public class User {
         nickname = "Пользователь "+userCounter.toString();
         userCounter++;
     }
+
     public String getNickname() {
         return nickname;
     }
+
     public int getBalance() {
         return money;
     }
+
+    public Spectator getSpectator() {
+        return spectator;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
     public void reduceBalance(int amount){
         money -= amount;
     }
+
     public UUID getUUID() {
         return uuid;
     }
+
+
     /**
      * We need setters in order to convert JSON to objects via reflection
      */
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public void setSpectator(Spectator spectator) {
+        this.spectator = spectator;
+    }
+
     public void setNickname(String nickname) {
         this.nickname = nickname;
     }
+
     public void setMoney(int money) {
         this.money = money;
     }
+
     public void updateData(Room room, Spectator sp) {
         Game game = room.getGame();
+        Player player = sp.getUser().getPlayer();
         TableStateUpdateMessage tsum = new TableStateUpdateMessage();
         TableStateData tsd = new TableStateData();
 
-       /* String[] actions = game.getPossibleActions(sp);
-        Card[]   playerCards = (Card[])game.getMyCards(sp).toArray();
-        Card[]   tableCards = (Card[])game.getTableCards(sp).toArray();
+        // User-specific data
+        String[] actions = null;
+        Card[]   playerCards = null;
 
-        List<Player> pl = game.getPlayers();
-        TableStateData.Player[] players = new TableStateData.Player[pl.size()];
-        for(int i = 0; i < pl.size(); i++) {
-            Player p = pl.get(i);
-            players[i].setID(p.getUUID().toString());
-            players[i].setName(p.getNickname());
-            players[i].setStake(game.getStake(sp));
-            players[i].setTurn(game.isMyTurn(sp));
+        if(player != null) {
+            // Get player actions
+            List<PlayerAction> actionsList = player.getActions();
+            actions = new String[actionsList.size()];
+            for(int i = 0; i < actionsList.size(); i++) {
+                actions[i] = actionsList.get(i).toString();
+            }
+
+            // Get player cards
+            List<Card> playerCardsList = player.getCards();
+            playerCards = new Card[playerCardsList.size()];
+            for(int i = 0; i < playerCardsList.size(); i++) {
+                playerCards[i] = playerCardsList.get(i);
+            }
         }
 
+        // Common players' data
+        Card[] tableCards = null;
+        TableStateData.Player[] players = null;
+
+        // Get table cards
+        List<Card> tableCardsList = game.getTableCards();
+        tableCards = tableCardsList.toArray(new Card[tableCardsList.size()]);
+
+        // Get players info
+        List<Player> playersList = game.getPlayers();
+        players = new TableStateData.Player[playersList.size()];
+        for(int i = 0; i < playersList.size(); i++) {
+            Player p = playersList.get(i);
+            players[i].setID(p.getId());
+            players[i].setName(p.getNickname());
+            players[i].setStake(game.getPlayerStake(player));
+            players[i].setState(p.getState().toString());
+        }
+
+        // Fill data object
         tsd.setActionList(actions);
-        tsd.setOverallStakes(game.getOverallStakes());
+        tsd.setOverallStakes(game.getAllStakes());
         tsd.setPlayerCards(playerCards);
         tsd.setTableCards(tableCards);
-        tsd.setPlayers(players);*/
+        tsd.setPlayers(players);
+
 
         tsum.setData(tsd);
-
         client.sendEvent(MessageType.STATE_UPDATE, tsum);
     }
 }
